025de77bf5772f34a92591c567ba67233c5bfe7f
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2015-10-13 17:37:33 -0600

    Fixed bug causing enormous changes in pR^2
    Turns out that the random jitter I was putting in before regression was too
    high; making the jitter smaller helped bring the pR^2 into good numbers, but the
    run time has increased.
    
    I also attempted making use of Gibbs sampling instead of ICM, but I couldn't get
    the topic word counts to work out correctly.

--- activetm/models.py
+++ activetm/models.py
@@ -26,6 +26,7 @@ def build(rng, settings):
                 NUM_TRAIN, NUM_SAMPLES_TRAIN, TRAIN_BURN, TRAIN_LAG,
                 NUM_SAMPLES_PREDICT, PREDICT_BURN, PREDICT_LAG)
     elif settings['model'] == 'sup_anchor':
+        tech.sampler.slda.set_seed(int(settings['cseed']))
         NUM_TOPICS = int(settings['numtopics'])
         NUM_TRAIN = int(settings['numtrain'])
         return factory['sup_anchor'](rng, NUM_TOPICS, NUM_TRAIN)
