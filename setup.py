dc57bbdcc881c4480cc6ea5edec8fde643802d52
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2016-03-04 10:07:46 -0700

    Added setup.py to make installation via pip easier

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''Setup for ActiveTM'''
+
+from distutils.core import setup
+
+setup(
+    name='activetm',
+    version='0.1',
+    description='Active learning with topic models',
+    license='MIT License',
+    author='Nozomu Okuda',
+    author_email='Nozomu.Okuda@gmail.com',
+    url='https://github.com/nOkuda/activetm',
+    packages=['activetm']
+)
