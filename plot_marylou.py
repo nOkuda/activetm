7dc81a326bf97b367936c963a815c05682e1302e
Nozomu Okuda <Nozomu.Okuda@byu.edu>
2016-10-20 14:07:43 -0600

    Added plotting code from supercomputer to repo

---
+++ plot_marylou.py
@@ -0,0 +1,45 @@
+"""Plots results for experiment on super computer"""
+
+import argparse
+import os
+
+import main
+from activetm import utils
+
+
+def parse_arguments():
+    """get commandline arguments"""
+    parser = argparse.ArgumentParser(prog='Results Plotter',
+                                     description='Plots results from '
+                                         'supercomputer')
+    parser.add_argument('resultspath', help='File path to results')
+    parser.add_argument(
+        'deltatxt',
+        help='Text file listing corpus with desired loss delta for generalized'\
+            + ' zero-one loss')
+    return parser.parse_args()
+
+
+def run():
+    """plot results from given experiment"""
+    args = parse_arguments()
+    corpora = [
+        'amazon',
+        'frus',
+        'sotu_broken',
+        'yelp'
+    ]
+    selections = [
+        'random',
+        'distance',
+        'balanced'
+    ]
+    for corpus in corpora:
+        main.make_plots(
+            os.path.join(args.resultspath, corpus),
+            selections,
+            utils.parse_settings(args.deltatxt))
+
+
+if __name__ == '__main__':
+    run()
